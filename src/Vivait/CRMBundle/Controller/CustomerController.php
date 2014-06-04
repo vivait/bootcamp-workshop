eaf3fba92e7b4c89ce9790985ade7646da522460
Kiel Goodman <kiel@vivait.co.uk>
2014-06-04 15:58:29 +0100

    Persisted a customer submitted from a form

--- src/Vivait/CRMBundle/Controller/CustomerController.php
+++ src/Vivait/CRMBundle/Controller/CustomerController.php
@@ -3,6 +3,7 @@
 namespace Vivait\CRMBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Vivait\CRMBundle\Entity\Customer;
 
 class CustomerController extends Controller
@@ -13,25 +14,39 @@ class CustomerController extends Controller
         $customers = $em->getRepository('VivaitCRMBundle:Customer')
             ->findAll();
 
-        return $this->render('VivaitCRMBundle:Customer:list.html.twig', [
-                'customers'=>$customers
-            ]);
+        return $this->render(
+            'VivaitCRMBundle:Customer:list.html.twig',
+            [
+                'customers' => $customers
+            ]
+        );
     }
 
-    public function addAction() {
+    public function addAction(Request $request)
+    {
         $customer = new Customer();
         //Full way
         //$form = $this->container->get('form.factory')->createBuilder('form', $customer, []);
         $form = $this->createFormBuilder($customer)
-            ->add('forename','text')
-            ->add('surname','text')
-            ->add('submit','submit')
+            ->add('forename', 'text')
+            ->add('surname', 'text')
+            ->add('submit', 'submit')
             ->getForm();
 
-        return $this->render('VivaitCRMBundle:Customer:form.html.twig', [
-                'form'=>$form->createView()
-            ]);
+        $form->handleRequest($request);
+        if($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($customer);
+            $em->flush();
+            return $this->redirect($this->generateUrl('vivait_crm_customer_list'));
+        }
 
+        return $this->render(
+            'VivaitCRMBundle:Customer:form.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
 
 
     }
