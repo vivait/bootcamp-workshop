846a26ffe4c9f4559d8e3b3b65176dac60d7e8aa
Kiel Goodman <kiel@vivait.co.uk>
2014-06-04 15:23:49 +0100

    Created a new customer entity and empty repository using the wizard

---
+++ src/Vivait/CRMBundle/Entity/Customer.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Vivait\CRMBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Customer
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Vivait\CRMBundle\Entity\CustomerRepository")
+ */
+class Customer
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="forename", type="string", length=255)
+     */
+    private $forename;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=255)
+     */
+    private $surname;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set forename
+     *
+     * @param string $forename
+     * @return Customer
+     */
+    public function setForename($forename)
+    {
+        $this->forename = $forename;
+
+        return $this;
+    }
+
+    /**
+     * Get forename
+     *
+     * @return string 
+     */
+    public function getForename()
+    {
+        return $this->forename;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     * @return Customer
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string 
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+}
