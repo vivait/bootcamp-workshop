f62a07cf0bd9a184bc535d8c7af1adbd9de1139e
Kiel Goodman <kiel@vivait.co.uk>
2014-06-04 15:51:56 +0100

    Created a form that can be used to submit new customers details

--- src/Vivait/CRMBundle/Controller/CustomerController.php
+++ src/Vivait/CRMBundle/Controller/CustomerController.php
@@ -3,11 +3,36 @@
 namespace Vivait\CRMBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Vivait\CRMBundle\Entity\Customer;
 
 class CustomerController extends Controller
 {
     public function listAction()
     {
-        return $this->render('VivaitCRMBundle:Customer:list.html.twig', []);
+        $em = $this->getDoctrine()->getManager();
+        $customers = $em->getRepository('VivaitCRMBundle:Customer')
+            ->findAll();
+
+        return $this->render('VivaitCRMBundle:Customer:list.html.twig', [
+                'customers'=>$customers
+            ]);
+    }
+
+    public function addAction() {
+        $customer = new Customer();
+        //Full way
+        //$form = $this->container->get('form.factory')->createBuilder('form', $customer, []);
+        $form = $this->createFormBuilder($customer)
+            ->add('forename','text')
+            ->add('surname','text')
+            ->add('submit','submit')
+            ->getForm();
+
+        return $this->render('VivaitCRMBundle:Customer:form.html.twig', [
+                'form'=>$form->createView()
+            ]);
+
+
+
     }
 }
